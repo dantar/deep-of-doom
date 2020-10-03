e79b4c6696879bd5f9d243f8869a8c8f60dc4408
Daniele <d.tarini@hyperborea.com>
2020-10-03 19:42:57 +0200

    Log action slot on click

--- src/app/components/fight-mob/fight-mob.component.ts
+++ src/app/components/fight-mob/fight-mob.component.ts
@@ -118,6 +118,10 @@ export class FightMobComponent implements OnInit {
     return `scale(${scale})`
   }
 
+  clickSlot(slot: ActionSlot) {
+    console.log(slot);
+  }
+
 }
 
 class ActionSlotBuilder {
